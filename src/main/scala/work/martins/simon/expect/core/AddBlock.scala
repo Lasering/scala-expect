e1d64ef23a30ac71166369de36ccf837c29acc4c
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2015-11-03 12:08:00 +0000

    Fix documentation.

--- src/main/scala/work/martins/simon/expect/core/AddBlock.scala
+++ src/main/scala/work/martins/simon/expect/core/AddBlock.scala
@@ -2,9 +2,9 @@ package work.martins.simon.expect.core
 
 trait AddBlock {
   /**
-   * Execute arbitrary actions upon this ${this.type}.
-   * @param block the block of code that executes upon this ${this.type}.
-   * @return this ${this.type}.
+   * Execute arbitrary actions upon the instance where this is mixed.
+   * @param block the block of code to execute.
+   * @return the current instance (this) of the type where this trait was mixed (this.type).
    */
   def addBlock(block: this.type => Unit): this.type = {
     block(this)
